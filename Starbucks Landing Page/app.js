0b5cef818e1c7bc1b8faa8894ab250eee731a44d
Sagyam Thapa <sagyamthapa32@gmail.com>
2020-12-08 14:01:30 +0545

    added: changing hero image

---
+++ Starbucks Landing Page/app.js
@@ -0,0 +1,7 @@
+function changeImgAndColor(imageName, color){
+    const circle = document.querySelector('.circle');
+    // const button = document.querySelector('.call-to-action');
+    // button.style.background = color;
+    circle.style.background = color;
+    document.querySelector('.hero-img').src = imageName 
+}
